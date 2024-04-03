38f81c371bf11db8488bf930c90a86e635d4cd1b
kinotikm <cloutkevin254@gmail.com>
2024-04-03 11:32:52 +0300

    modify initial commits

--- grade.js
+++ grade.js
@@ -1,14 +1,22 @@
+//define a function called calculateGrade
 function calculateGade(score){
+    //check if score is greater than 79
   
     if (score > 79){
         return'A';
+        //if its greater ,return 'A'
     }
+    //if score is not greterthan 79 ,will check between 60and 79
     else if (score >= 60 && score <= 79){
         return'B';
+        //if true return 'B'
     }
+    //if score is ! between 60 &&& 79 ,it checks if score is between 50 and 59(inclusive)
     else if (score >= 50 && score <= 59){
         return'C';
+        //if true it returns 'c'
     }
+    //if score is !between 50 and 59 it checks 40 and 59 (inclusive)
     else if (grade >= 40 && score <=49){
         return'D';
     }
@@ -17,5 +25,6 @@ function calculateGade(score){
     
     }
     }
+    //define a variable called score then assign a value 79
     score =79
     console.log(`The Student Grade Is:`,calculateGade(score));
--- speed_detector.js
+++ speed_detector.js
@@ -1,20 +1,30 @@
+//define a funnctioncalled calculateDemeritPoints with a parameter called speed
 function calculateDemeritPoints(speed) {
+    //define a constant variable called speed limit then set to 70
     const speedLimit = 70;
+    //a variable known as demeritPoints and set it to 0
     let demeritPoints = 0;
-
+//check if speed is less than or equal to speed limit
     if (speed <= speedLimit) {
         console.log("Ok");
+        //if true .logs "ok"
     } else {
+        //if speed exceeds speed limit ,calculate the demerits ponts
         demeritPoints = Math.floor((speed - speedLimit) / 5);
+        //log the calculated demerits
         console.log(`Points: ${demeritPoints}`);
     }
+    //check if demerits exceed 12
 
     if (demeritPoints > 12) {
+        //if true driver's license will be suspended
         console.log("License suspended");
     }
+    
+      return demeritPoints;
 
-    return demeritPoints;
+}
+    //Define a variable called speed and asign a value of 80
 
-
-}var speed =80;
+var speed =80;
 calculateDemeritPoints(speed);
